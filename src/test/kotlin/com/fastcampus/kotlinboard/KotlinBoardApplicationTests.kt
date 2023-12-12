4832173eb2e49ec22ca83a53b9d8f926c9f3c87c
hmkim829 <hmkim829@gmail.com>
2023-12-12 22:36:20 +0900

    initial commit

---
+++ src/test/kotlin/com/fastcampus/kotlinboard/KotlinBoardApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.fastcampus.kotlinboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
